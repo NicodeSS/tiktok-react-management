97bd05098c7551145c64327f449dc53f76b1d0b7
NicodeSS <RD_Nicode@outlook.com>
2021-02-22 23:27:53 +0800

    chore: update API interface

--- src/types/live.d.ts
+++ src/types/live.d.ts
@@ -1,11 +1,14 @@
 export interface LiveInfo {
     _id: string,
+    living: boolean,
     publishUrl: string,
     playUrl: string,
     description: string,
     author_id: string,
     author_nick: string,
-    author_avatar: string
+    author_avatar: string,
+    createdAt: string,
+    updatedAt: string
 }
 
 export interface LiveInfo_Create {
